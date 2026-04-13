a7565f10c69240f5ce4784800ea447b5faae5b0d
Tien Nguyen <ndtien212@gmail.com>
2026-04-13 19:40:44 +0700

    update service user-manager add FeignClients and add config server cho mall-service

--- user-manager/src/main/java/vmall/user_manager/UserManagerApplication.java
+++ user-manager/src/main/java/vmall/user_manager/UserManagerApplication.java
@@ -2,7 +2,9 @@ package vmall.user_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class UserManagerApplication {
 
